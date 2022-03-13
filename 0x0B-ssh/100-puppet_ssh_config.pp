60b8920b951e588568a2b20a6bc24e5e47c9cbf8
Evans262 <evancechapuma62@gmail.com>
2022-03-13 11:58:52 +0200

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # This manifest changes the ssh configuration file
 exec {'/etc/ssh/ssh_config':
 path     => '/bin',
-command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+command  => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
 provider => 'shell',
 }
