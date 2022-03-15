1a37533fb3a07f891144867e5160bf09420230fd
Evans262 <evancechapuma62@gmail.com>
2022-03-15 23:40:49 +0200

    installing with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,2 +1,34 @@
 # Puppet maniest that installs NGINX
-class{"nginx": }
+# Puppet maniest that installs NGINX
+'nginx-plus': {
+  apt::source { 'nginx':
+    location => "https://plus-pkgs.nginx.com/${distro}",
+    repos    => 'nginx-plus',
+    key      => '573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62',
+  }
+  exec { 'download 90nginx file':
+  command => "/usr/bin/wget -q -O /etc/apt/apt.conf.d/90nginx
+https://cs.nginx.com/static/files/90nginx",
+  creates => "/etc/apt/apt.conf.d/90nginx"
+  }
+  file { '/etc/ssl/nginx':
+    ensure => directory,
+    mode => "0755",
+    owner => "root",
+    group => "root",
+  }
+  file { '/etc/ssl/nginx/nginx-repo.crt':
+    ensure => present,
+    mode => "0644",
+    owner => "root",
+    group => "root",
+    content => "Contents of your nginx‑repo.crt file here as one line",
+  }
+  file { '/etc/ssl/nginx/nginx-repo.key':
+    ensure => present,
+    mode => "0644",
+    owner => "root",
+    group => "root",
+    content => "Contents of your nginx‑repo.key file here as one line",
+  }
+}
