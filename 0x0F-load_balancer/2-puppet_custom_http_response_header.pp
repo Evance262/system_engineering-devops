709641ca8cc53c836804de2a12ca713034eb5d08
Evans262 <evancechapuma62@gmail.com>
2022-03-23 23:52:13 +0200

    a puppet is here

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# puppet config nginx
+
+exec { '/usr/bin/env apt-get -y update' : }
+package { 'nginx':
+  ensure => installed,
+}
+file { '/var/www/html/index.html' :
+  content => 'Hello World!',
+}
+-> file_line { 'header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+-> exec { 'Restaring Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
