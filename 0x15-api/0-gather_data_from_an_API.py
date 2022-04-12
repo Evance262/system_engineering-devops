fae21c3e6af00f20fbae84317ec46896c8f9bec5
Evans262 <evancechapuma62@gmail.com>
2022-04-12 15:54:57 +0200

    get data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-RESTful API to get data
-"""
+""" a Python script that, using a REST API, for a given employee ID,
+    returns information about his/her TODO list progress."""
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    # api endpoint
-    api_url = "https://jsonplaceholder.typicode.com/"
-
-    # getting user fron the api
-    users = requests.get("https://jsonplaceholder.typicode.com/user/{}"
-                         .format(sys.argv[1])).json()
-
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(sys.argv[1])).json()
-
-    # checks if a task is completed
-    finished_tasks = [task.get("title") for task in todos if
-                      task.get("completed") is True]
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        users.get("name"), len(finished_tasks), len(todos)))
-
-    [print("\t {}".format(finished)) for finished in finished_tasks]
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    r = requests.get(url)
+    name = r.json().get("name")
+    url2 = 'https://jsonplaceholder.typicode.com/todos'
+    r2 = requests.get(url2)
+    task_names = []
+    tasks = 0
+    completed = 0
+    for item in r2.json():
+        if item.get("userId") == int(sys.argv[1]):
+            tasks += 1
+            if item.get("completed") is True:
+                completed += 1
+                task_names.append(item.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          completed,
+                                                          tasks))
+    for i in task_names:
+        print("\t {}".format(i))
