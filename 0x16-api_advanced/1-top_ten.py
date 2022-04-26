e1837a2cf7cebd1c22f44c6b4c78dfea706c3e2b
Evans262 <evancechapuma62@gmail.com>
2022-04-27 00:22:31 +0200

    Top ten Reddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ def top_ten(subreddit):
     prints the titles of the first 10 hot posts
     """
     user = {"User-Agent": "RasberyyCalm762"}
-    request = requests.get("https://www.reddit.com/r/{}/about.json"
+    request = requests.get("https://www.reddit.com/r/{}/hot/.json?limit=10"
                            .format(subreddit), headers=user)
 
     if request.status_code == 200:
