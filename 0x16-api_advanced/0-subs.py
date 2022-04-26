81583c1cf243de39ca9cb553afc86616a3f72a10
Evans262 <evancechapuma62@gmail.com>
2022-04-26 23:56:17 +0200

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API 
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    '''Returns 0 if an invalid
+    subreddit is given'''
+
+    user = {"User-Agent": "RasberyyCalm762"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
+    else:
+        return 0
---
+++ 0x16-api_advanced/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+import requests
+
+# def number_of_subscribers(subreddit):
+#     '''Returns 0 if an invalid
+#     subreddit is given'''
+#     subreddit = requests.get("")
